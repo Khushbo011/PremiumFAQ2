7696a69769963c31116dd9427b4d071ea76076fa
brijeshyogi024 <brijeshyogi024@gmail.com>
2026-06-17 17:29:49 +0530

    Fix TypeError: static import build/server/index.js in api/index.js to resolve routes undefined crash

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import { createRequestHandler } from "react-router";
+import * as build from "../build/server/index.js";
 
 // Vercel serverless function entry point
 export default createRequestHandler({
-  build: () => import("../build/server/index.js"),
+  build,
   mode: process.env.NODE_ENV ?? "production",
 });
